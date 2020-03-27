7d99db8c21a4c9502f4b219795015b5ef39f1605
Rahul <holdmypotion@gmial.com>
2020-03-27 10:04:48 +0530

    Added the custon model with superuser

--- app/core/models.py
+++ app/core/models.py
@@ -16,6 +16,15 @@ class UserManger(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that uses email instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,12 @@ class ModelTests(TestCase):
         """Test that creating user with invalid email rasise error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test34')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'tesf-2347'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
